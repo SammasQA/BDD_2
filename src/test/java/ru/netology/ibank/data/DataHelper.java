f44e172ca13ea9eaf878e103df7277c9b7d948b0
Sergey_Mashinin <smashinin@mail.ru>
2026-03-28 13:02:18 +0300

    исправления замечаний_2

---
+++ src/test/java/ru/netology/ibank/data/DataHelper.java
@@ -0,0 +1,24 @@
+package ru.netology.ibank.data;
+
+public class DataHelper {
+
+    public static String getValidLogin() {
+        return "vasya";
+    }
+
+    public static String getValidPassword() {
+        return "qwerty123";
+    }
+
+    public static String getValidVerificationCode() {
+        return "12345";
+    }
+
+    public static String getFirstCardNumber() {
+        return "5559 0000 0000 0001";
+    }
+
+    public static String getSecondCardNumber() {
+        return "5559 0000 0000 0002";
+    }
+}
--- src/test/java/ru/netology/ibank/steps/TransferSteps.java
+++ src/test/java/ru/netology/ibank/steps/TransferSteps.java
@@ -1,9 +1,9 @@
 package ru.netology.ibank.steps;
 
-import com.codeborne.selenide.Configuration;
 import io.cucumber.java.ru.Когда;
 import io.cucumber.java.ru.Пусть;
 import io.cucumber.java.ru.Тогда;
+import ru.netology.ibank.data.DataHelper;
 import ru.netology.ibank.page.DashboardPage;
 import ru.netology.ibank.page.LoginPage;
 import ru.netology.ibank.page.TransferPage;
@@ -16,33 +16,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TransferSteps {
     private static final String SUT_URL = "http://localhost:9999";
-    private static final String VERIFICATION_CODE = "12345";
 
     private DashboardPage dashboardPage;
 
     @Пусть("пользователь залогинен с именем {string} и паролем {string}")
     public void login(String login, String password) {
-        Configuration.headless = Boolean.parseBoolean(System.getProperty("selenide.headless", "false"));
-
         open(SUT_URL);
         LoginPage loginPage = new LoginPage();
-        VerificationPage verificationPage = loginPage.validLogin(login, password);
-        dashboardPage = verificationPage.validVerify(VERIFICATION_CODE);
+        VerificationPage verificationPage = loginPage.validLogin(
+                login,
+                password
+        );
+        dashboardPage = verificationPage.validVerify(DataHelper.getValidVerificationCode());
         webdriver().shouldHave(url(SUT_URL + "/dashboard"));
     }
 
-    @Когда("пользователь переводит {int} рублей с карты с номером {string} на свою 1 карту с главной страницы")
-    public void transfer(int amount, String fromCardNumber) {
-        // Нажимаем "Пополнить" на первой карте (получатель)
-        TransferPage transferPage = dashboardPage.clickTransferButton(0);
-        // Указываем карту-отправитель
+    @Когда("пользователь переводит {int} рублей с карты с номером {string} на карту с номером {string}")
+    public void transfer(int amount, String fromCardNumber, String toCardNumber) {
+        TransferPage transferPage = dashboardPage.clickTransferButton(toCardNumber);
         dashboardPage = transferPage.transfer(amount, fromCardNumber);
     }
 
-    @Тогда("баланс его 1 карты из списка на главной странице должен стать {int} рублей")
-    public void verifyBalance(int expectedBalance) {
-        int actualBalance = dashboardPage.getCardBalance(0);
+    @Тогда("баланс карты с номером {string} должен стать {int} рублей")
+    public void verifyBalance(String cardNumber, int expectedBalance) {
+        int actualBalance = dashboardPage.getCardBalance(cardNumber);
         assertEquals(expectedBalance, actualBalance,
-                "Баланс первой карты не соответствует ожидаемому");
+                "Баланс карты " + cardNumber + " не соответствует ожидаемому");
     }
 }
